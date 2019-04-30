17efb50d39b7da26a09934a96a26ea346d19499d
Rodriguez <764092@cognizant.com>
2019-04-30 16:27:47 +0200

    adding time travel

--- src/board.js
+++ src/board.js
@@ -3,85 +3,32 @@ import Square from './square';
 
 export default class Board extends React.Component {
 
-    /* constructor(props) {
-        super(props);
-        this.state = {
-            history: [{
-                squares: Array(9).fill(null),
-              }],
-            xIsNext: true,
-        };
-    }*/
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (this.calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        console.log(squares);
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-
     renderSquare(i) {
-        return <Square 
+        return (
+          <Square
             value={this.props.squares[i]}
-            onClick={() => this.props.handleClick(i)}
-        />;
+            onClick={() => this.props.onClick(i)}
+          />
+        );
     }
-
-    calculateWinner(squares) {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-        }
-        return null;
-    }
-
 
     render() {
-        /* const history = this.state.history;
-        const current = history[history.length - 1];*/ 
-        const winner = this.calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
         return (
         <div>
-            <div className="status">{status}</div>
             <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
             </div>
             <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
             </div>
             <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
             </div>
         </div>
         );
